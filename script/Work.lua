ea9eef3876b1c0a7100668f9b47d5d32f7a6d458
JustAww <98517992+JustAww@users.noreply.github.com>
2023-04-17 15:50:26 +0200

    Update Work.lua

--- script/Work.lua
+++ script/Work.lua
@@ -6,7 +6,6 @@ HundredHardDash.Size     = Vector3.new(500000, 4, 500000)
 HundredHardDash.Anchored = true
 HundredHardDash.Transparency = 1
 
-
 local YourCar = function()
     local Car = nil;
     for i,v in pairs(workspace.Vehicles:GetChildren()) do
@@ -17,14 +16,37 @@ local YourCar = function()
     return Car
 end;
 
+local Customer = function(car)
+    local Target = nil;
+    for i,v in pairs(workspace.NewCustomers:GetChildren()) do
+        if car and v:FindFirstChild'Part' and v.Part:GetAttribute('Rating') >= RatingTarget and v.Part:GetAttribute('Rating') <= HighestRatingTarget then
+            Target = v;
+        end
+    end
+    return Target
+end;
 
 while true do
     local PlayerCar = YourCar();
-
+    if PlayerCar and PlayerCar:FindFirstChild'REAL' and PlayerCar.REAL:FindFirstChild'DRIFT' then
+        
+        local TargetCustomer = Customer(PlayerCar);
+        if TargetCustomer and TargetCustomer:FindFirstChild'Part' and not workspace.ParkingMarkers:FindFirstChild'destinationPart' then
+            PlayerCar:PivotTo(TargetCustomer.Part.CFrame)
+            
+            if (PlayerCar.REAL.DRIFT.Position - TargetCustomer.Part.Position).Magnitude <= 10 then
+                
+                game.VirtualInputManager:SendKeyEvent(1, Enum.KeyCode.R, 0, game)
+                if workspace.ParkingMarkers:FindFirstChild'destinationPart' then
+                    game.VirtualInputManager:SendKeyEvent(0, Enum.KeyCode.R, 0, game)
+                end
+            end
+        end;
+        
         if workspace.ParkingMarkers:FindFirstChild'destinationPart' then
             local Destination = workspace.ParkingMarkers.destinationPart;
             
-            HundredHardDash.CFrame = Destination.CFrame * CFrame.new(0,0,0)
+            HundredHardDash.CFrame = Destination.CFrame * CFrame.new(0,1,0)
             
             if (PlayerCar.REAL.DRIFT.Position - Destination.Position).Magnitude >= 20 then
                 PlayerCar:PivotTo(HundredHardDash.CFrame * CFrame.new(0,6,0))
